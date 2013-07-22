0064bfeaf7dd62acc4a259bbb919904b614868dd
vdeparday <vivien.deparday@gmail.com>
2013-07-22 21:08:04 +0530

    Update Vagrant file for last vagrant version and set up bridge network with static ip

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Setting up the vagrant box
 Change the ip in the VagrantFile to a valid ip on the local network 
 
 	vagrant up
-
+	vagrant ssh
 The basic box will be downloaded automatically and will be provisioned with GeoNode (see the bash script)
 
 	geonode createsuperuser
--- provision_geonode.sh
+++ provision_geonode.sh
@@ -2,10 +2,13 @@
 
 set -x
 
+ifconfig eth1 192.168.100.52 netmask 255.255.255.0 up
+
 which geonode > /dev/null &&
 { echo "Geonode already installed."; exit 0; }
 
 add-apt-repository ppa:geonode/testing
 apt-get update
+apt-get dist-upgrade
 apt-get install -y geonode
 geonode-updateip www.riskinfo.lk
