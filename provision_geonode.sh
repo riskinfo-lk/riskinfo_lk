70004461da12c22cf5e32ebb2e807a8496936876
Disaster Management Centre <dmc@Geonode.dmc>
2013-07-22 21:49:32 +0530

    Fix provision script to preinstall python-software-properties

--- provision_geonode.sh
+++ provision_geonode.sh
@@ -7,8 +7,9 @@ ifconfig eth1 192.168.100.52 netmask 255.255.255.0 up
 which geonode > /dev/null &&
 { echo "Geonode already installed."; exit 0; }
 
+apt-get install -y python-software-properties
 add-apt-repository ppa:geonode/testing
 apt-get update
-apt-get dist-upgrade
+apt-get dist-upgrade -y
 apt-get install -y geonode
 geonode-updateip www.riskinfo.lk
