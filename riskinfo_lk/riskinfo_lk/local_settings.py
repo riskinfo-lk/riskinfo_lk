e8863999a80b9d1093fb01d57c8d2a200f681d5d
vdeparday <vivien.deparday@gmail.com>
2013-12-03 23:06:16 +0530

    Hook language switcher button

--- riskinfo_lk/riskinfo_lk/local_settings.py
+++ riskinfo_lk/riskinfo_lk/local_settings.py
@@ -8,7 +8,21 @@ DEBUG_STATIC = False
 
 LANGUAGES = (
     ('en', 'English'),
-    ('es', 'Spanish'),
+    ('si', 'Sinhala'),
 )
+
+EXTRA_LANG_INFO = {
+    'si': {
+        'bidi': False,
+        'code': 'si',
+        'name': 'Sinhala',
+        'name_local': u'සිංහල', #unicode codepoints here
+    },
+}
+
+# Monkey patch to add custom languages not provided by Django
+import django.conf.locale
+LANG_INFO = dict(django.conf.locale.LANG_INFO.items() + EXTRA_LANG_INFO.items())
+django.conf.locale.LANG_INFO = LANG_INFO
 
 LOCKDOWN_GEONODE = True
