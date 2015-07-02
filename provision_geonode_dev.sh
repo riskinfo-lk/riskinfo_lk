ea61aadf720d06fdf461ac287d39dc9e66a6bf1c
Jeffrey Johnson <ortelius@gmail.com>
2015-07-02 11:49:37 -0700

    fix error in provision script

--- provision_geonode_dev.sh
+++ provision_geonode_dev.sh
@@ -4,7 +4,7 @@ which virtualenv > /dev/null &&
 { echo "Already provisioned"; exit 0; }
 
 sudo apt-get update
-sudo apt-get -y install python python-pip git apt-get install zlib1g-dev openjdk-7-jdk python-dev python-lxml gdal-bin build-essential libxml2-dev libxslt-dev
+sudo apt-get -y install python python-pip git zlib1g-dev openjdk-7-jdk python-dev python-lxml gdal-bin build-essential libxml2-dev libxslt-dev
 
 sudo pip install virtualenvwrapper
 source /usr/local/bin/virtualenvwrapper.sh
