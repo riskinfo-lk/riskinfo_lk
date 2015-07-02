8e9a4ea94e5ef7716253bf9e217ebb51019bbc26
Jeffrey Johnson <ortelius@gmail.com>
2015-07-02 07:12:20 -0700

    Quick fixes for 2.4

--- riskinfo_lk/riskinfo_lk/urls.py
+++ riskinfo_lk/riskinfo_lk/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from geonode.urls import *
 
 urlpatterns = patterns('',
-
-    # Static pages
-    url(r'^$', 'geonode.views.index', {'template': 'site_index.html'}, name='home'),
+   url(r'^/?$',
+       TemplateView.as_view(template_name='site_index.html'),
+       name='home'),
  ) + urlpatterns
